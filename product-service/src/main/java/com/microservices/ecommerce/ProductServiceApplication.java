32227ac27573c9ff0f05ac19349a9c2aceec6453
Mert Bahardogan <mert.bahardogan@icterra.com>
2021-11-20 15:12:43 +0300

    first commit

---
+++ product-service/src/main/java/com/microservices/ecommerce/ProductServiceApplication.java
@@ -0,0 +1,44 @@
+package com.microservices.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.ArrayList;
+
+@SpringBootApplication
+@Configuration
+public class ProductServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProductServiceApplication.class, args);
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis( RequestHandlerSelectors.basePackage( "com.microservices.ecommerce"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Spring Boot Simple REST API",
+                "Spring Boot Simple REST API Swagger Documentation",
+                "Version 1",
+                "urn:tos",
+                new Contact("Admin", "www.mertbahardogan.com", "info@mertbahardogan.com"),
+                "Apache 2.0",
+                "https://www.apache.org/licenses/LICENSE-2.0",
+                new ArrayList<>());
+    }
+}
---
+++ product-service/src/main/java/com/microservices/ecommerce/product/service/api/ProductsController.java
@@ -0,0 +1,7 @@
+package com.microservices.ecommerce.product.service.api;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductsController {
+}
