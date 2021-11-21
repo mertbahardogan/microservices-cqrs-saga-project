706d1cdee5b79f5ffb36d0d3e26a127d09c65de8
mertbahardogan <mbahardogan@hotmail.com>
2021-11-22 00:12:43 +0300

    Eureka Server added.

--- product-service/src/main/java/com/microservices/ecommerce/ProductServiceApplication.java
+++ product-service/src/main/java/com/microservices/ecommerce/ProductServiceApplication.java
@@ -24,7 +24,7 @@ public class ProductServiceApplication {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis( RequestHandlerSelectors.basePackage( "com.microservices.ecommerce"))
+                .apis( RequestHandlerSelectors.basePackage("com.microservices.ecommerce"))
                 .paths(PathSelectors.any())
                 .build()
                 .apiInfo(apiInfo());
--- product-service/src/main/java/com/microservices/ecommerce/product/service/api/ProductsController.java
+++ product-service/src/main/java/com/microservices/ecommerce/product/service/api/ProductsController.java
@@ -1,7 +1,15 @@
 package com.microservices.ecommerce.product.service.api;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/api")
 @RestController
 public class ProductsController {
+
+    @GetMapping("products")
+    public String products(){
+        return "All products";
+    }
 }
