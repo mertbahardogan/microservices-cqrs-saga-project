5dbc381c6d05d79892344d69f449780644a173cf
mertbahardogan <mbahardogan@hotmail.com>
2022-08-30 15:08:45 +0300

    unused codes deleted.

--- gateway-server/src/main/java/com/microservices/ecommerce/swagger/SwaggerProvider.java
+++ gateway-server/src/main/java/com/microservices/ecommerce/swagger/SwaggerProvider.java
@@ -3,7 +3,6 @@ package com.microservices.ecommerce.swagger;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.microservices.ecommerce.config.GatewayConfig;
 import com.microservices.ecommerce.config.SwaggerServicesConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
@@ -18,18 +17,12 @@ import springfox.documentation.swagger.web.SwaggerResourcesProvider;
 @Slf4j
 public class SwaggerProvider implements SwaggerResourcesProvider {
 
-    //  TODO:buna dinamik olarak eriştiğimizde olacak gibi - daha sonra önüne ayrım amaçlı bir ad vererel urlleştirmemiz gerekir eğer çalışırsa.
-    //  TODO: 9001 portu yerine load balance yapılarak dinamik port alınmalı. Bu ayağa kalkan lb alınarak yapılabilir.
+    private final SwaggerServicesConfig swaggerServiceList;
 
     @Autowired
-    private SwaggerServicesConfig swaggerServiceList;
-//    private final GatewayConfig gatewayServiceList;
-//
-//    @Autowired
-//    public SwaggerProvider(SwaggerServicesConfig swaggerServiceList, GatewayConfig gatewayServiceList) {
-//        this.swaggerServiceList = swaggerServiceList;
-//        this.gatewayServiceList = gatewayServiceList;
-//    }
+    public SwaggerProvider(SwaggerServicesConfig swaggerServiceList) {
+        this.swaggerServiceList = swaggerServiceList;
+    }
 
     @Override
     public List<SwaggerResource> get() {
@@ -39,9 +32,6 @@ public class SwaggerProvider implements SwaggerResourcesProvider {
             resources.add(swaggerResource(service.getName(), service.getUrl(), service.getVersion()));
         });
 
-//        gatewayServiceList.getServices().forEach(gatewayService -> {
-//            System.out.println(gatewayService.getUri());
-//        });
         return resources;
     }
 
