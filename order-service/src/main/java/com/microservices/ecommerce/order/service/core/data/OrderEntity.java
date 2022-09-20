c02b45b631ccc3cf9b780d4260bbc7249c8966b1
mertbahardogan <mbahardogan@hotmail.com>
2022-09-20 23:05:45 +0300

    order entity fixed.

--- order-service/src/main/java/com/microservices/ecommerce/order/service/core/data/OrderEntity.java
+++ order-service/src/main/java/com/microservices/ecommerce/order/service/core/data/OrderEntity.java
@@ -1,12 +1,17 @@
 package com.microservices.ecommerce.order.service.core.data;
 
 import com.microservices.ecommerce.order.service.core.enums.OrderStatus;
+import lombok.Data;
 
 import javax.persistence.*;
+import java.io.Serializable;
 
+@Data
 @Entity
 @Table(name="orders")
-public class OrderEntity {
+public class OrderEntity implements Serializable {
+
+    private static final long serialVersionUID = 5313493413859894403L;
 
     @Id
     @Column(unique = true)
